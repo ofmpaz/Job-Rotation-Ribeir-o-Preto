39aa840f9c2df139837c54e54d299aed2f27bca2
Maria Andrade <102066058+ofmpaz@users.noreply.github.com>
2023-03-29 17:18:30 -0300

    Add files via upload

---
+++ Questao_05.java
@@ -0,0 +1,28 @@
+package JobRotationRibeirãoPreto;
+
+import java.util.Scanner;
+public class Questao_05 {
+
+	public static void main(String[] args) {
+
+
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Digite uma palavra: ");
+		String str = sc.nextLine();
+
+		String strInvertida = "";
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+			strInvertida += str.charAt(i);
+		}
+
+		System.out.println("String invertida: " + strInvertida);
+
+		sc.close();
+	}
+}
+
+
+
