e6cd1a4a301530e4098a1237c66bf0432803f87a
Maria Andrade <102066058+ofmpaz@users.noreply.github.com>
2023-03-29 16:51:26 -0300

    Add files via upload
    Este código Java foi desenvolvido para resolver a questão 01 do processo seletivo da empresa Job Rotation Ribeirão Preto.
    
    Resolução:
    O programa calcula a soma dos primeiros "n" números naturais, onde "n" é representado pela variável "indice". No código fornecido, "indice" foi definido como 13.
    
    O programa utiliza uma estrutura de repetição "while" para somar cada número de 1 até "indice". A variável "k" é utilizada para controlar a quantidade de números somados e "soma" é utilizada para armazenar a soma total.
    
    O resultado é exibido no console através do método "System.out.println()".

---
+++ Questao_01.java
@@ -0,0 +1,21 @@
+package JobRotationRibeirãoPreto;
+
+public class Questao_01 {
+
+	public static void main(String[] args) {
+
+		int indice = 13, soma = 0, k = 0; 
+
+
+		while(k < indice) {
+
+			k = k + 1; 
+
+			soma = soma + k; 
+		}
+
+		System.out.println(soma);
+
+	}
+
+}
